0cbe0fadd4d7b585d7f77005bdb7055dc565d217
Tada Teruki <me@peruki.dev>
2024-11-08 09:53:16 +0900

    環境整備

---
+++ src/typy/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+os.putenv("XDG_RUNTIME_DIR", "/tmp/.cache/xdgr")
--- src/typy/__main__.py
+++ src/typy/__main__.py
@@ -2,7 +2,18 @@ import pygame as pg
 
 
 def main():
-    print("Hello, world!")
+    pg.init()
+    screen = pg.display.set_mode((800, 600))
+    pg.display.set_caption("Typy")
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                return
+        screen.fill((100, 250, 250))
+        pg.draw.circle(screen, (250, 100, 100), (400, 300), 100)
+        pg.display.flip()
 
 
 if __name__ == "__main__":
